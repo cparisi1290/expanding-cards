f1bc79d40aea8ef333d73e22ba9052690cf54fdf
Cassandra <cparisi1290@gmail.com>
2021-08-06 14:39:50 -0500

    select all panels, iterate over them remove active class from whichever panel is active then add active class to panel that was clicked

---
+++ script.js
@@ -0,0 +1,19 @@
+// puts panels in a node list, similar to an array, with querySelectorAll
+const panels = document.querySelectorAll('.panel');
+
+// console.log(panels[0]);
+
+panels.forEach((panel) => {
+  //   console.log(panel);
+
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
